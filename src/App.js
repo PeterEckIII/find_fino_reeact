c1e5c26f10ce14cbb36198661bb197e1fdb6046a
Peter Eck <jpeckiii@gmail.com>
2019-05-17 13:34:27 -0500

    Implement Header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 import Vendors from "./components/Vendors";
-import { vendorList } from "./data/vendors";
-import './App.css';
+import { illinoisVendorList, marylandVendorList } from "./data/vendors";
+import Header from "./components/Header";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <Vendors vendorList={vendorList} />
+      <Header title="Illinois-Based Vendors" />
+      <Vendors vendorList={illinoisVendorList} />
+      <Header title="Maryland & DC-Based Vendors" />
+      <Vendors vendorList={marylandVendorList} />
     </div>
   );
-}
+};
 
 export default App;
