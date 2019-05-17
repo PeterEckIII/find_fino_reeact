89498e70c18222e1f1e84f72d1352e898df07ec0
Peter Eck <jpeckiii@gmail.com>
2019-05-17 13:35:46 -0500

    Formatting changes

--- src/components/Vendors.jsx
+++ src/components/Vendors.jsx
@@ -1,52 +1,45 @@
-import React from 'react'
+import React from "react";
 import VendorCard from "./VendorCard";
 import styled from "styled-components";
 
 const VendorList = styled.ul`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
 
-    @media screen and (max-width: 1140px) {
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-    }
+  @media screen and (max-width: 1140px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
 
-    @media screen and (max-width: 930px) {
-        grid-template-columns: 1fr 1fr 1fr;
-    }
+  @media screen and (max-width: 930px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 
-    @media screen and (max-width: 690px) {
-        grid-template-columns: 1fr 1fr;
-    }
-    @media screen and (max-width: 460px) {
-        grid-template-columns: 1fr;
-    }
+  @media screen and (max-width: 690px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media screen and (max-width: 460px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
-const Vendors = (props) => {
-    return (
-        <div style={{
-            "display": "grid",
-            "grid-template-columns": "1fr 1fr 1fr 1fr 1fr",
-            "@media screen and (max-width: 1140px)": "{ grid-template-columns: 1fr 1fr 1fr 1fr; }",
-            "@media screen and (max-width: 930px)": "{ grid-template-columns: 1fr 1fr 1fr; }",
-            "@media screen and (max-width: 690px)": "{ grid-template-columns: 1fr 1fr;}",
-            "@media screen and (max-width: 460px": "{ grid-template-columns: 1fr }"
-        }}>
-            {props.vendorList.map(vendor => {
-                return (
-                    <VendorCard 
-                        key={vendor.address}
-                        name={vendor.name}
-                        type={vendor.type} 
-                        address={vendor.address}
-                        city={vendor.city} 
-                        state={vendor.state} 
-                        zipCode={vendor.zipCode}
-                    />
-                )
-            })}
-        </div>
-    )
-}
+const Vendors = props => {
+  return (
+    <VendorList>
+      {props.vendorList.map(vendor => {
+        return (
+          <VendorCard
+            key={vendor.address}
+            name={vendor.name}
+            type={vendor.type}
+            address={vendor.address}
+            city={vendor.city}
+            state={vendor.state}
+            zipCode={vendor.zipCode}
+          />
+        );
+      })}
+    </VendorList>
+  );
+};
 
 export default Vendors;
